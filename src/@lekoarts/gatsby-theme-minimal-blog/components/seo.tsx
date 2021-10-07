ea560156afdeaaefbb8c73b1d305b20a9ebb6cca
jennybehan <jennybe0117@gmail.com>
2021-10-07 22:13:54 +0900

    remove: hotjar

--- src/@lekoarts/gatsby-theme-minimal-blog/components/seo.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/seo.tsx
@@ -60,17 +60,6 @@ const SEO = ({
       <link rel="icon" type="image/png" sizes="32x32" href={withPrefix(`/favicon-32x32.png`)} />
       <link rel="icon" type="image/png" sizes="16x16" href={withPrefix(`/favicon-16x16.png`)} />
       <link rel="apple-touch-icon" sizes="180x180" href={withPrefix(`/apple-touch-icon.png`)} />
-      <script>{`
-        (function(h,o,t,j,a,r){
-          h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-          h._hjSettings={hjid:2522399,hjsv:6};
-          a=o.getElementsByTagName('head')[0];
-          r=o.createElement('script');r.async=1;
-          r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-          a.appendChild(r);
-        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-      `}
-      </script>
       {canonicalUrl ? <link rel="canonical" href={canonicalUrl} /> : null}
       {children}
     </Helmet>
