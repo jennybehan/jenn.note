16a72835593a396153d6b551826c7f2d6e5da7a7
한주연 <juyeon.han@juyeonhan01-2.local>
2024-01-15 14:00:12 +0900

    한 번 더 수정

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,6 +75,7 @@ module.exports = {
     `gatsby-plugin-offline`,
     `gatsby-plugin-netlify`,
     `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
     shouldAnalyseBundle && {
       resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
       options: {
